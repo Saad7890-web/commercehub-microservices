e157e1770738db9f9f3ba9c69d61c35a8ab28193
Saad7890-web <omysaadislam@gmail.com>
2026-01-30 15:51:44 +0600

    wire with gRPC

--- auth-service/internal/delivery/grpc/server.go
+++ auth-service/internal/delivery/grpc/server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 
 	"auth-service/auth-service/pkg/pb"
+	"auth-service/internal/usecase"
 
 	"google.golang.org/grpc"
 )
@@ -13,12 +14,14 @@ import (
 type Server struct {
 	grpcServer *grpc.Server
 	port       string
+	authUC     usecase.AuthUsecase
 }
 
-func NewServer(port string) *Server {
+func NewServer(port string, authUC usecase.AuthUsecase) *Server {
 	return &Server{
 		grpcServer: grpc.NewServer(),
 		port:       port,
+		authUC:     authUC,
 	}
 }
 
@@ -28,7 +31,7 @@ func (s *Server) Start() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	handler := NewAuthHandler()
+	handler := NewAuthHandler(s.authUC)
 	pb.RegisterAuthServiceServer(s.grpcServer, handler)
 
 	log.Println("gRPC server listening on port", s.port)
