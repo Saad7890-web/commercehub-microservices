76aa0b632a5dfd63eb00ecae184a0cb76f38372c
Saad7890-web <omysaadislam@gmail.com>
2026-01-30 15:48:06 +0600

    added handler

--- auth-service/internal/delivery/grpc/handler.go
+++ auth-service/internal/delivery/grpc/handler.go
@@ -4,22 +4,32 @@ import (
 	"context"
 
 	"auth-service/auth-service/pkg/pb"
+	"auth-service/internal/usecase"
 )
 
 type AuthHandler struct {
 	pb.UnimplementedAuthServiceServer
+	authUsecase usecase.AuthUsecase
 }
 
-func NewAuthHandler() *AuthHandler {
-	return &AuthHandler{}
+func NewAuthHandler(authUsecase usecase.AuthUsecase) *AuthHandler {
+	return &AuthHandler{
+		authUsecase: authUsecase,
+	}
 }
 
 func (h *AuthHandler) Register(
 	ctx context.Context,
 	req *pb.RegisterRequest,
 ) (*pb.RegisterResponse, error) {
+
+	err := h.authUsecase.Register(req.Email, req.Password)
+	if err != nil {
+		return nil, err
+	}
+
 	return &pb.RegisterResponse{
-		Message: "register endpoint not implemented",
+		Message: "user registered successfully",
 	}, nil
 }
 
@@ -27,7 +37,13 @@ func (h *AuthHandler) Login(
 	ctx context.Context,
 	req *pb.LoginRequest,
 ) (*pb.LoginResponse, error) {
+
+	token, err := h.authUsecase.Login(req.Email, req.Password)
+	if err != nil {
+		return nil, err
+	}
+
 	return &pb.LoginResponse{
-		AccessToken: "",
+		AccessToken: token,
 	}, nil
 }
